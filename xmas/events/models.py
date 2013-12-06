dirn/Secret-Santa

commit 2963e4a58f77d84a116fc68d5c269a151c3cce08

Allow users to claim specific quantities
Well I'm not so proud of a lot of the code here, but this is a much
needed feature. I can come back and clean it up later.

The basic idea is that when someone asks for multiple of something (but
not unlimited), a user should be able to claim any number of the item
that is still available. Allowing this to happen requires some changes
to `Item.claim()`, the `frontend.events.claim` view, the JavaScript, and
the template, and the CSS.

And boy do I need to start writing tests for my models.

xmas/events/models.py
@@ -186,18 +186,25 @@ class Item(db.Model):
     def claim(self, user, quantity=1):
         """Claim an item."""
         if self.is_claimed(user.id):
-            # Change the claim.
+            claim = ItemClaim.query.filter(
+                ItemClaim.item_id == self.id,
+                ItemClaim.user_id == user.id,
+            ).one()
+        else:
+            claim = ItemClaim(item=self, user=user, quantity=0)
+            self.claims.append(claim)
+
+        attempted_quantity = min(quantity - claim.quantity, self.quantity_remaining)
+        if not attempted_quantity:
+            db.session.rollback()
             return
 
-        if self.quantity and self.quantity_claimed + quantity > self.quantity:
-            # Raise an exception.
-            return
+        claim.quantity += attempted_quantity
 
         db.session.query(Item).filter(Item.id == self.id).update({
-            Item.quantity_claimed: Item.quantity_claimed + quantity,
+            Item.quantity_claimed: Item.quantity_claimed + attempted_quantity,
         })
 
-        self.claims.append(ItemClaim(self, user, quantity))
         db.session.commit()
 
     def is_claimed(self, user_id):
@@ -235,6 +242,21 @@ class Item(db.Model):
             ).one()
             claim.purchased = False
             db.session.commit()
+
+    def quantity_claimed_by_user(self, user_id):
+        """Return the quantity claimed by a user."""
+        if not self.is_claimed(user_id):
+            return 0
+
+        claim = ItemClaim.query.filter(
+            ItemClaim.item_id == self.id,
+            ItemClaim.user_id == user_id,
+        ).one()
+        return claim.quantity
+
+    @property
+    def quantity_remaining(self):
+        return self.quantity - self.quantity_claimed
 
     def unclaim(self, user):
         """Remove a claim from an item."""

xmas/frontend/events.py
@@ -25,7 +25,9 @@ def claim():
     if not event.active:
         abort(404)
 
-    item.claim(current_user)
+    quantity = max(int(request.form.get('quantity', 0)), 1)
+
+    item.claim(current_user, quantity)
     return render_template('events/item.html', item=item)
 
 

xmas/frontend/static/css/screen.css
@@ -1,1 +1,1 @@
-body{padding-top:50px;position:relative}.xmas-main-nav{background-color:#bf0000;border-color:#a70000;box-shadow:0 1px 0 rgba(255,255,255,0.1);text-shadow:0 -1px 0 rgba(0,0,0,0.15)}.xmas-main-nav .navbar-brand{color:#fff}.xmas-main-nav .navbar-collapse{border-color:#a70000}.xmas-main-nav .navbar-toggle{border-color:#bf0000}.xmas-main-nav .navbar-nav>li>a{color:#eeb0b0}.xmas-main-nav .navbar-nav>.active a,.xmas-main-nav .navbar-nav>.active a:focus,.xmas-main-nav .navbar-nav>.active a:hover{background-color:#a70000;color:#fff}.xmas-header{background-color:#bf0000;color:#eeb0b0;padding:30px 15px 40px;text-shadow:0 1px 0 rgba(0,0,0,0.15)}@media screen and (min-width: 768px){.xmas-header{font-size:21px;text-align:left}}.xmas-header .container{position:relative}.xmas-header h1{color:#fff}@media screen and (min-width: 768px){.xmas-header h1{font-size:60px;line-height:1}}@media screen and (min-width: 992px){.xmas-header h1{margin-right:380px}}.xmas-header p{font-weight:300;line-height:1.5}.xmas-sidenav{background-color:#f7f5fa;border-radius:5px;margin-bottom:30px;margin-top:30px;padding-bottom:10px;padding-top:10px;text-shadow:0 1px 0 #fff}@media screen and (min-width: 992px){.xmas-sidebar.affix .xmas-sidenav{margin-bottom:0;margin-top:0}}.xmas-sidebar.affix{position:static}@media screen and (min-width: 992px){.xmas-sidebar.affix{position:fixed;top:80px;width:213px}}@media screen and (min-width: 1200px){.xmas-sidebar.affix{width:263px}}.xmas-sidebar .nav>li>a{color:#716b7a;display:block;padding:5px 20px}.xmas-sidebar .nav>.active>a,.xmas-sidebar .nav>.active:focus>a,.xmas-sidebar .nav>.active:hover>a{background-color:transparent;border-right:1px solid #bf0000;color:#bf0000;font-weight:bold}h2[id]{margin-top:-45px;padding-top:80px}.xmas-section .page-header{overflow:hidden}.xmas-section .page-header img{float:right}.xmas-details{background-color:#fafafa;border-color:#e5e5e5 #eee #eee;border-style:solid;border-width:1px 0;box-shadow:inset 0 3px 6px rgba(0,0,0,0.05);margin:0 -15px 15px;padding:45px 15px 15px;position:relative}@media screen and (min-width: 768px){.xmas-details{background-color:#fff;border-color:#ddd;border-radius:4px 4px 0 0;border-width:1px;box-shadow:none;margin-left:0;margin-right:0}}.xmas-details:after{content:'I want...';color:#bbb;font-size:12px;font-weight:bold;left:15px;letter-spacing:1px;position:absolute;text-transform:uppercase;top:15px}.xmas-details h3{margin-top:0}.xmas-details h3 .quantity{color:#bbb;font-size:75%;font-weight:normal}.xmas-details .btn-group{margin:9px 0 5px}.xmas-details p:last-child{color:#bf0000;font-style:italic;text-indent:10px}.xmas-details+.highlight{border-radius:0;border-width:0 0 1px;margin:-15px -15px 15px}@media screen and (min-width: 768px){.xmas-details+.highlight{border-bottom-left-radius:4px;border-bottom-right-radius:4px;border-width:1px;margin-left:0;margin-right:0;margin-top:-16px}}.highlight{background-color:#f7f7f9;border:1px solid #e1e1e8;border-radius:4px;display:none;margin-bottom:14px;padding:9px 14px}@media screen and (min-width: 768px){.highlight{display:block}}.highlight p{background-color:transparent;border:0;margin-bottom:0;margin-top:0;padding:0}.xmas-wishlist{padding-top:30px}.xmas-wishlist label{display:none}.xmas-wishlist p label{cursor:pointer;display:inline-block;margin-right:4px}.xmas-claims{padding-top:30px}.xmas-claims .xmas-details:after{content:"I'm getting..."}.xmas-profile{padding-top:30px}.xmas-profile label{display:none}.xmas-login{padding-top:30px}.xmas-login label{display:none}.xmas-login label[for=remember]{display:inline-block}.xmas-messages{background-color:#fafafa;border-bottom:1px solid #e5e5e5;border-top:1px solid #fff;color:#777;padding:15px 20px}.xmas-messages .alert{border:0;border-radius:0;margin-bottom:0;margin-top:5px;padding:0}.xmas-messages .alert:first-child{margin:0}.xmas-messages .alert-info{background-color:#fafafa;color:#777}.xmas-error{padding-top:30px}
+body{padding-top:50px;position:relative}.xmas-main-nav{background-color:#bf0000;border-color:#a70000;box-shadow:0 1px 0 rgba(255,255,255,0.1);text-shadow:0 -1px 0 rgba(0,0,0,0.15)}.xmas-main-nav .navbar-brand{color:#fff}.xmas-main-nav .navbar-collapse{border-color:#a70000}.xmas-main-nav .navbar-toggle{border-color:#bf0000}.xmas-main-nav .navbar-nav>li>a{color:#eeb0b0}.xmas-main-nav .navbar-nav>.active a,.xmas-main-nav .navbar-nav>.active a:focus,.xmas-main-nav .navbar-nav>.active a:hover{background-color:#a70000;color:#fff}.xmas-header{background-color:#bf0000;color:#eeb0b0;padding:30px 15px 40px;text-shadow:0 1px 0 rgba(0,0,0,0.15)}@media screen and (min-width: 768px){.xmas-header{font-size:21px;text-align:left}}.xmas-header .container{position:relative}.xmas-header h1{color:#fff}@media screen and (min-width: 768px){.xmas-header h1{font-size:60px;line-height:1}}@media screen and (min-width: 992px){.xmas-header h1{margin-right:380px}}.xmas-header p{font-weight:300;line-height:1.5}.xmas-sidenav{background-color:#f7f5fa;border-radius:5px;margin-bottom:30px;margin-top:30px;padding-bottom:10px;padding-top:10px;text-shadow:0 1px 0 #fff}@media screen and (min-width: 992px){.xmas-sidebar.affix .xmas-sidenav{margin-bottom:0;margin-top:0}}.xmas-sidebar.affix{position:static}@media screen and (min-width: 992px){.xmas-sidebar.affix{position:fixed;top:80px;width:213px}}@media screen and (min-width: 1200px){.xmas-sidebar.affix{width:263px}}.xmas-sidebar .nav>li>a{color:#716b7a;display:block;padding:5px 20px}.xmas-sidebar .nav>.active>a,.xmas-sidebar .nav>.active:focus>a,.xmas-sidebar .nav>.active:hover>a{background-color:transparent;border-right:1px solid #bf0000;color:#bf0000;font-weight:bold}h2[id]{margin-top:-45px;padding-top:80px}.xmas-section .page-header{overflow:hidden}.xmas-section .page-header img{float:right}.xmas-details{background-color:#fafafa;border-color:#e5e5e5 #eee #eee;border-style:solid;border-width:1px 0;box-shadow:inset 0 3px 6px rgba(0,0,0,0.05);margin:0 -15px 15px;padding:45px 15px 15px;position:relative}@media screen and (min-width: 768px){.xmas-details{background-color:#fff;border-color:#ddd;border-radius:4px 4px 0 0;border-width:1px;box-shadow:none;margin-left:0;margin-right:0}}.xmas-details:after{content:'I want...';color:#bbb;font-size:12px;font-weight:bold;left:15px;letter-spacing:1px;position:absolute;text-transform:uppercase;top:15px}.xmas-details h3{margin-top:0}.xmas-details h3 .quantity{color:#bbb;font-size:75%;font-weight:normal}.xmas-details .xmas-quantity-claimed label,.xmas-details .xmas-quantity-remaining label{font-weight:normal}.xmas-details .xmas-quantity-claimed input,.xmas-details .xmas-quantity-remaining input{display:inline;text-align:center;width:70px}.xmas-details .btn-group{margin:9px 0 5px}.xmas-details p:last-child{color:#bf0000;font-style:italic;text-indent:10px}.xmas-details+.highlight{border-radius:0;border-width:0 0 1px;margin:-15px -15px 15px}@media screen and (min-width: 768px){.xmas-details+.highlight{border-bottom-left-radius:4px;border-bottom-right-radius:4px;border-width:1px;margin-left:0;margin-right:0;margin-top:-16px}}.highlight{background-color:#f7f7f9;border:1px solid #e1e1e8;border-radius:4px;display:none;margin-bottom:14px;padding:9px 14px}@media screen and (min-width: 768px){.highlight{display:block}}.highlight p{background-color:transparent;border:0;margin-bottom:0;margin-top:0;padding:0}.xmas-wishlist{padding-top:30px}.xmas-wishlist label{display:none}.xmas-wishlist p label{cursor:pointer;display:inline-block;margin-right:4px}.xmas-claims{padding-top:30px}.xmas-claims .xmas-details:after{content:"I'm getting..."}.xmas-profile{padding-top:30px}.xmas-profile label{display:none}.xmas-login{padding-top:30px}.xmas-login label{display:none}.xmas-login label[for=remember]{display:inline-block}.xmas-messages{background-color:#fafafa;border-bottom:1px solid #e5e5e5;border-top:1px solid #fff;color:#777;padding:15px 20px}.xmas-messages .alert{border:0;border-radius:0;margin-bottom:0;margin-top:5px;padding:0}.xmas-messages .alert:first-child{margin:0}.xmas-messages .alert-info{background-color:#fafafa;color:#777}.xmas-error{padding-top:30px}

xmas/frontend/static/js/global-ck.js
@@ -1,1 +1,1 @@
-!function(e){var t=e(window),n=e(document.body),r=e(".navbar").outerHeight(!0)+10;n.scrollspy({target:".xmas-sidebar",offset:r});t.on("load",function(){n.scrollspy("refresh")});e(".xmas-container [href=#]").click(function(e){e.preventDefault()});setTimeout(function(){var t=e(".xmas-sidebar");t.affix({offset:{top:function(){var n=t.offset().top,r=parseInt(t.children(0).css("margin-top"),10),i=e(".xmas-main-nav").height();return this.top=n-i-r}}})},100);setTimeout(function(){e(".xmas-top").affix()},100);e(".xmas-section").on("click","button",function(){var t=e(this),n=t.data("id"),r="",i="POST";if(t.hasClass("xmas-claim"))r="/claim";else if(t.hasClass("xmas-unclaim"))r="/unclaim";else if(t.hasClass("xmas-purchase")){r="/purchase";i="PUT"}else if(t.hasClass("xmas-return")){r="/return";i="PUT"}e.ajax({url:r,type:i,data:{id:n}}).done(function(e){t.hasClass("xmas-unclaim")&&t.closest(".xmas-section").hasClass("xmas-claims")?t.closest(".xmas-item").fadeOut("slow"):t.closest(".xmas-item").replaceWith(e)})});e(".xmas-wishlist").on("click","button",function(t){var n=e(this),r=n.closest(".xmas-item");if(n.hasClass("btn-add"))return;if(n.hasClass("btn-cancel")){r.find("form").remove();r.find(".btn-group").show();return}t.preventDefault();var s=n.closest(".xmas-wishlist").find("form").attr("action");s+="/"+n.data("id");var o="",u={},a=null;if(n.hasClass("btn-edit")){o="GET";a=function(e){r.find(".btn-group").hide();r.append(e);i(r.find("input[name=quantity]"))}}else if(n.hasClass("btn-update")){o="PUT";u=n.closest("form").serialize();a=function(e){if(e=="OK"){r.find("form").remove();r.find(".btn-group").show()}else r.html(e)}}else if(n.hasClass("btn-delete")){if(!confirm("Are you sure you want to delete "+r.find("h3").html()+"?"))return;o="DELETE";a=function(){r.parent().remove()}}e.ajax({url:s,type:o,data:u}).done(a)});var i=function(t){var n=e(t),r=n.closest("form").find("input[name=id]").val(),i=e("<p/>");i.insertAfter(n.parent());var s=e('<label for="qty-unlimited-'+r+'">Give me a lot!</label>');i.append(s);var o=e('<input type="checkbox" name="unlimited" id="qty-unlimited-'+r+'">');i.append(o);o.change(function(){if(o.is(":checked")){o.data("original",n.val());n.hide();n.val(0)}else{n.val(o.data("original"));n.show()}});if(n.val()==="0"){o.attr("checked",!0);o.change()}};e("input[name=quantity]").each(function(){i(this)})}(window.jQuery);
+!function(e){var t=e(window),n=e(document.body),r=e(".navbar").outerHeight(!0)+10;n.scrollspy({target:".xmas-sidebar",offset:r});t.on("load",function(){n.scrollspy("refresh")});e(".xmas-container [href=#]").click(function(e){e.preventDefault()});setTimeout(function(){var t=e(".xmas-sidebar");t.affix({offset:{top:function(){var n=t.offset().top,r=parseInt(t.children(0).css("margin-top"),10),i=e(".xmas-main-nav").height();return this.top=n-i-r}}})},100);setTimeout(function(){e(".xmas-top").affix()},100);e(".xmas-section").on("click","button",function(){var t=e(this),n=t.data("id"),r=1,i="",s="POST";if(t.hasClass("xmas-claim")){i="/claim";var o=t.closest(".xmas-details").find("input[name=quantity]");o.length&&(r=o.val())}else if(t.hasClass("xmas-unclaim"))i="/unclaim";else if(t.hasClass("xmas-purchase")){i="/purchase";s="PUT"}else if(t.hasClass("xmas-return")){i="/return";s="PUT"}e.ajax({url:i,type:s,data:{id:n,quantity:r}}).done(function(e){t.hasClass("xmas-unclaim")&&t.closest(".xmas-section").hasClass("xmas-claims")?t.closest(".xmas-item").fadeOut("slow"):t.closest(".xmas-item").replaceWith(e)})});e(".xmas-wishlist").on("click","button",function(t){var n=e(this),r=n.closest(".xmas-item");if(n.hasClass("btn-add"))return;if(n.hasClass("btn-cancel")){r.find("form").remove();r.find(".btn-group").show();return}t.preventDefault();var s=n.closest(".xmas-wishlist").find("form").attr("action");s+="/"+n.data("id");var o="",u={},a=null;if(n.hasClass("btn-edit")){o="GET";a=function(e){r.find(".btn-group").hide();r.append(e);i(r.find("input[name=quantity]"))}}else if(n.hasClass("btn-update")){o="PUT";u=n.closest("form").serialize();a=function(e){if(e=="OK"){r.find("form").remove();r.find(".btn-group").show()}else r.html(e)}}else if(n.hasClass("btn-delete")){if(!confirm("Are you sure you want to delete "+r.find("h3").html()+"?"))return;o="DELETE";a=function(){r.parent().remove()}}e.ajax({url:s,type:o,data:u}).done(a)});var i=function(t){var n=e(t),r=n.closest("form").find("input[name=id]").val(),i=e("<p/>");i.insertAfter(n.parent());var s=e('<label for="qty-unlimited-'+r+'">Give me a lot!</label>');i.append(s);var o=e('<input type="checkbox" name="unlimited" id="qty-unlimited-'+r+'">');i.append(o);o.change(function(){if(o.is(":checked")){o.data("original",n.val());n.hide();n.val(0)}else{n.val(o.data("original"));n.show()}});if(n.val()==="0"){o.attr("checked",!0);o.change()}};e(".xmas-wishlist input[name=quantity]").each(function(){i(this)})}(window.jQuery);

xmas/frontend/static/js/global.js
@@ -40,10 +40,16 @@
     $('.xmas-section').on('click', 'button', function() {
         var $this = $(this);
         var id = $this.data('id');
+        var quantity= 1;
 
         var url = '', method = 'POST';
         if ($this.hasClass('xmas-claim')) {
             url = '/claim';
+
+            var $quantity= $this.closest('.xmas-details').find('input[name=quantity]');
+            if ($quantity.length) {
+                quantity= $quantity.val();
+            }
         } else if ($this.hasClass('xmas-unclaim')) {
             url = '/unclaim';
             // TODO: Change the method to DELETE.
@@ -59,7 +65,7 @@
         $.ajax({
             url: url,
             type: method,
-            data: {id: id}
+            data: {id: id, quantity: quantity}
         }).done(function(html) {
             if ($this.hasClass('xmas-unclaim') && $this.closest('.xmas-section').hasClass('xmas-claims')) {
                 $this.closest('.xmas-item').fadeOut('slow');
@@ -155,7 +161,7 @@
         }
     };
 
-    $('input[name=quantity]').each(function() {
+    $('.xmas-wishlist input[name=quantity]').each(function() {
         initQuantity(this);
     });
 }(window.jQuery)

xmas/frontend/static/sass/screen.scss
@@ -186,6 +186,19 @@ h2[id] {
     }
   }
 
+  .xmas-quantity-claimed,
+  .xmas-quantity-remaining {
+    label {
+      font-weight: normal;
+    }
+
+    input {
+      display: inline;
+      text-align: center;
+      width: 70px;
+    }
+  }
+
   .btn-group {
     margin: 9px 0 5px;
   }

xmas/frontend/templates/events/item.html
@@ -6,10 +6,34 @@
         <span class="quantity">(this x {{ item.quantity }})</span>
       {% endif %}
     </h3>
+
     {% if item.description %}<p>{{ item.description }}</p>{% endif %}
+
+    {% if item.quantity > 1 and (item.quantity_remaining or item.is_claimed(current_user.id)) %}
+      <div class="form-group xmas-quantity-remaining">
+        <label class="control-label" for="quantity-remaining-{{ item.id }}">
+          {{ item.recipient.user.name }} wants
+        </label>
+        <input type="text" name="quantity_remaining" id="quantity-remaining-{{ item.id }}" value="{{ item.quantity_remaining }}" class="form-control" readonly>
+        more.
+      </div>
+      <div class="form-group xmas-quantity-claimed">
+        <label class="control-label" for="quantity-{{ item.id }}">
+          I'm getting
+        </label>
+        {% set quantity_claimed = item.quantity_claimed_by_user(current_user.id) %}
+        <input type="number" name="quantity" id="quantity-{{ item.id }}" value="{{ quantity_claimed }}" min="1" max="{{ quantity_claimed + item.quantity_remaining }}" class="form-control">
+      </div>
+    {% endif %}
+
     <div class="btn-group">
       {% if item.is_claimed(current_user.id) %}
         <button type="button" class="btn btn-default xmas-unclaim" data-id="{{ item.id }}">Nah, I changed my mind</button>
+
+        {% if item.quantity > 1 %}
+          <button type="button" class="btn btn-default xmas-claim" data-id="{{ item.id }}">I want to change the number I'm getting</button>
+        {% endif %}
+
         {% if item.is_purchased(current_user.id) %}
           <button type="button" class="btn btn-default xmas-return" data-id="{{ item.id }}">I was wrong, I haven't purchased this item yet</button>
         {% else %}
@@ -19,6 +43,7 @@
         <button type="button" class="btn btn-default xmas-claim" data-id="{{ item.id }}">I want to get this!</button>
       {% endif %}
     </div>
+
     {% set number_of_claims = item.claims|length - (1 if item.is_claimed(current_user.id) else 0) %}
     {% if number_of_claims %}
       <p>
@@ -32,6 +57,7 @@
       </p>
     {% endif %}
   </div>
+
   {% if item.url %}
     <div class="highlight">
       <a href="{{ item.url }}" target="_blank">Show it to me</a>

commit b10cbc83d3e5158725fa45f23dd1a5eda8326fb8

Restore ability to claim unlimited items
2963e4a introduced a regression that prevented users from claiming an
item if it has an unlimited quantity. Items with unlimited quantities
need to be handled differently when being claimed.

xmas/events/models.py
@@ -194,8 +194,20 @@ class Item(db.Model):
             claim = ItemClaim(item=self, user=user, quantity=0)
             self.claims.append(claim)
 
-        attempted_quantity = min(quantity - claim.quantity, self.quantity_remaining)
+        if self.quantity:
+            # If the item has a specified quantity, make sure the claim
+            # is for a portion of that quantity that is still available.
+            attempted_quantity = min(
+                quantity - claim.quantity, self.quantity_remaining
+            )
+        else:
+            # If an unlimited quantity was requested, only allow the
+            # claim to be for 1 unless the user has already claimed it.
+            # In that case, nothing additional should be claimed.
+            attempted_quantity = 0 if claim.quantity else 1
+
         if not attempted_quantity:
+            # If there is nothing to claim, get out.
             db.session.rollback()
             return
 