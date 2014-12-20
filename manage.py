4a708a3dba7ae885e54bd5324ffd1f17ec228707
Andy Dirnberger <dirn@dirnonline.com>
2014-12-19 19:26:30 -0500

    Use Flask-Migrate for migrations

--- manage.py
+++ manage.py
@@ -1,13 +1,19 @@
 """Management commands"""
 
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 
+from xmas.core import db
 from xmas.factory import create_app
 from xmas.manage import CreateEventCommand, CreateUserCommand
 
-manager = Manager(create_app(__name__, ''))
+app = create_app(__name__, '')
+migrate = Migrate(app, db)
+
+manager = Manager(app)
 manager.add_command('create_event', CreateEventCommand())
 manager.add_command('create_user', CreateUserCommand())
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
